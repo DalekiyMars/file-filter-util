de9c4dea251d9f552655ec968be05b14824d6cc9
DalekiyM4rs <evgeniybulychev@bk.ru>
2024-12-08 23:36:25 +0300

    Обработка флагов - для каждого вызывается свой метод

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    static Map<String, Runnable> flagActions = new HashMap<>(){{
+        put("-o", Main::changeOutputFileDirectory);
+        put("-p", Main::changeOutputFileName);
+        put("-s", Main::writeShortStatistics);
+        put("-f", Main::writeFullStatistics);
+        put("-a", Main::writeToOldFile);
+    }};
     public static void main(String[] args) {
+        for (String arg : args) {
+            Runnable action = flagActions.get(arg); // Получить действие по флагу
+            if (action != null) {
+                action.run(); // Выполнить действие
+            } else {
+                System.out.println("Unknown flag: " + arg);
+            }
+        }
+    }
+
+    public static void changeOutputFileDirectory(){
+
+    }
+
+    public static void changeOutputFileName(){
+
+    }
+
+    public static void writeShortStatistics(){
+
+    }
+
+    public static void writeFullStatistics(){
+
+    }
+
+    public static void writeToOldFile(){
 
     }
 }
